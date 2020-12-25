fa950767a1d8d278499ba72e06884d390a9efc19
Marcelo Arrarte <marcelo.arrarte.ma@gmail.com>
2020-12-25 17:13:16 -0300

    Initial commit

---
+++ Viborita/FrmJuego.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmJuego
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.btnIniciar = New System.Windows.Forms.Button()
+        Me.pnlCampoDeJuego = New System.Windows.Forms.Panel()
+        Me.tmrMovimiento = New System.Windows.Forms.Timer(Me.components)
+        Me.lblPuntaje = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(13, 415)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 0
+        Me.btnSalir.TabStop = False
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'btnIniciar
+        '
+        Me.btnIniciar.Location = New System.Drawing.Point(713, 415)
+        Me.btnIniciar.Name = "btnIniciar"
+        Me.btnIniciar.Size = New System.Drawing.Size(75, 23)
+        Me.btnIniciar.TabIndex = 1
+        Me.btnIniciar.TabStop = False
+        Me.btnIniciar.Text = "Iniciar"
+        Me.btnIniciar.UseVisualStyleBackColor = True
+        '
+        'pnlCampoDeJuego
+        '
+        Me.pnlCampoDeJuego.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pnlCampoDeJuego.Location = New System.Drawing.Point(13, 13)
+        Me.pnlCampoDeJuego.Name = "pnlCampoDeJuego"
+        Me.pnlCampoDeJuego.Size = New System.Drawing.Size(775, 396)
+        Me.pnlCampoDeJuego.TabIndex = 2
+        '
+        'tmrMovimiento
+        '
+        Me.tmrMovimiento.Interval = 250
+        '
+        'lblPuntaje
+        '
+        Me.lblPuntaje.AutoSize = True
+        Me.lblPuntaje.Location = New System.Drawing.Point(357, 420)
+        Me.lblPuntaje.Name = "lblPuntaje"
+        Me.lblPuntaje.Size = New System.Drawing.Size(55, 13)
+        Me.lblPuntaje.TabIndex = 3
+        Me.lblPuntaje.Text = "Puntaje: 0"
+        Me.lblPuntaje.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.lblPuntaje.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(476, 420)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Label1"
+        '
+        'FrmJuego
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblPuntaje)
+        Me.Controls.Add(Me.pnlCampoDeJuego)
+        Me.Controls.Add(Me.btnIniciar)
+        Me.Controls.Add(Me.btnSalir)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.KeyPreview = True
+        Me.Name = "FrmJuego"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents btnIniciar As Button
+    Friend WithEvents pnlCampoDeJuego As Panel
+    Friend WithEvents tmrMovimiento As Timer
+    Friend WithEvents lblPuntaje As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Viborita/FrmJuego.vb
@@ -0,0 +1,260 @@
+﻿Public Class FrmJuego
+    Private Enum TiposDireccion
+        Arriba
+        Derecha
+        Abajo
+        Izquierda
+    End Enum
+
+    ' Constantes para la configuración del juego
+    Private Const Paso As Integer = 16
+    Private Const IntervaloInicial As Integer = 250
+    Private Const BaseExponencialVelocidad As Double = 1.06
+
+    ' Variables usadas en el transcurso del juego
+    Private Property Direccion As TiposDireccion = TiposDireccion.Arriba
+    Private Property YaMovioEnEsteIntervalo As Boolean = False
+    Private Property JuegoEnCurso As Boolean = False
+    Private _puntaje As Integer = 0
+    Private Property Puntaje As Integer
+        Get
+            Return _puntaje
+        End Get
+        Set(value As Integer)
+            _puntaje = value
+            lblPuntaje.Text = "Puntaje: " & Puntaje
+            Dim factorVelocidad As Double = BaseExponencialVelocidad ^ Puntaje
+            tmrMovimiento.Interval = IntervaloInicial / factorVelocidad
+            Label1.Text = "Intervalo: " & tmrMovimiento.Interval
+        End Set
+    End Property
+
+    ' Referencias a objetos importantes en el juego
+    Private Property SeccionesCuerpo As New List(Of Label)
+    Private Property Cabeza As Label
+    Private Property Cola As Label
+    Private Property Comida As Label
+
+    Private Sub btnIniciar_Click(sender As Object, e As EventArgs) Handles btnIniciar.Click
+        pnlCampoDeJuego.Controls.Clear()
+        SeccionesCuerpo.Clear()
+        Direccion = TiposDireccion.Arriba
+        tmrMovimiento.Interval = IntervaloInicial
+
+        Cabeza = CrearSeccionCuerpo((pnlCampoDeJuego.Width - Paso) / 2, (pnlCampoDeJuego.Height - Paso) / 2)
+        AgregarSeccionCuerpo(Cabeza)
+
+        GenerarComida()
+
+        tmrMovimiento.Enabled = True
+        ActiveControl = Nothing
+        btnIniciar.Visible = False
+        lblPuntaje.Visible = True
+        Puntaje = 0
+    End Sub
+
+    Private Sub tmrMovimiento_Tick(sender As Object, e As EventArgs) Handles tmrMovimiento.Tick
+        YaMovioEnEsteIntervalo = False
+        Cola = SeccionesCuerpo.Last
+        Dim posicionCola As Point = Cola.Location
+
+        For i = SeccionesCuerpo.Count - 1 To 1 Step -1
+            Dim seccion As Label = SeccionesCuerpo(i)
+            seccion.Location = SeccionesCuerpo(i - 1).Location
+        Next
+
+        Select Case Direccion
+            Case TiposDireccion.Arriba
+                Cabeza.Location = New Point(Cabeza.Location.X, Cabeza.Location.Y - Paso)
+            Case TiposDireccion.Derecha
+                Cabeza.Location = New Point(Cabeza.Location.X + Paso, Cabeza.Location.Y)
+            Case TiposDireccion.Abajo
+                Cabeza.Location = New Point(Cabeza.Location.X, Cabeza.Location.Y + Paso)
+            Case TiposDireccion.Izquierda
+                Cabeza.Location = New Point(Cabeza.Location.X - Paso, Cabeza.Location.Y)
+        End Select
+
+        If ComioComida() Then
+            GenerarComida()
+            AgregarSeccionCuerpo(CrearSeccionCuerpo(posicionCola.X, posicionCola.Y))
+        End If
+
+        If ChocaConBordes() Or ChocaConsigo() Then
+            tmrMovimiento.Enabled = False
+            btnIniciar.Visible = True
+            btnIniciar.Text = "Reiniciar"
+            MsgBox("¡Se acabó el juego!" & vbNewLine & "Puntaje: " & Puntaje, MsgBoxStyle.Information, "¡Gran partida!")
+        End If
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        tmrMovimiento.Enabled = False
+        If MsgBox("¿Deseas salir del juego?", MsgBoxStyle.YesNo, "Salir") = MsgBoxResult.Yes Then
+            Close()
+        Else
+            tmrMovimiento.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Form1_PreviewKeyDown(sender As Object, e As PreviewKeyDownEventArgs) Handles MyBase.PreviewKeyDown
+        Select Case e.KeyCode
+            Case Keys.Up, Keys.Left, Keys.Down, Keys.Right
+                e.IsInputKey = True
+        End Select
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If JuegoEnCurso And Not YaMovioEnEsteIntervalo Then
+            YaMovioEnEsteIntervalo = True
+            Select Case e.KeyCode
+                Case Keys.W, Keys.Up
+                    If Direccion = TiposDireccion.Izquierda Or Direccion = TiposDireccion.Derecha Then
+                        Direccion = TiposDireccion.Arriba
+                    End If
+                    e.Handled = True
+                Case Keys.A, Keys.Left
+                    If Direccion = TiposDireccion.Abajo Or Direccion = TiposDireccion.Arriba Then
+                        Direccion = TiposDireccion.Izquierda
+                    End If
+                    e.Handled = True
+                Case Keys.S, Keys.Down
+                    If Direccion = TiposDireccion.Derecha Or Direccion = TiposDireccion.Izquierda Then
+                        Direccion = TiposDireccion.Abajo
+                    End If
+                    e.Handled = True
+                Case Keys.D, Keys.Right
+                    If Direccion = TiposDireccion.Arriba Or Direccion = TiposDireccion.Abajo Then
+                        Direccion = TiposDireccion.Derecha
+                    End If
+                    e.Handled = True
+                Case Else
+                    YaMovioEnEsteIntervalo = False
+            End Select
+        End If
+    End Sub
+
+    Private Sub btnIniciar_VisibleChanged(sender As Object, e As EventArgs) Handles btnIniciar.VisibleChanged
+        JuegoEnCurso = Not btnIniciar.Visible
+    End Sub
+
+    Private Sub btnSalir_GotFocus(sender As Object, e As EventArgs) Handles btnSalir.GotFocus
+        ActiveControl = Nothing
+    End Sub
+
+    Private Function ChocaConBordes() As Boolean
+        Return Cabeza.Location.X < 0 Or
+            Cabeza.Location.X + Paso > pnlCampoDeJuego.Width Or
+            Cabeza.Location.Y < 0 Or
+            Cabeza.Location.Y + Paso > pnlCampoDeJuego.Height
+    End Function
+
+    Private Function ChocaConsigo() As Boolean
+        For i = 4 To SeccionesCuerpo.Count - 1
+            Dim seccion As Label = SeccionesCuerpo(i)
+            Dim rectanguloSeccion As New Rectangle(seccion.Location, seccion.Size)
+            Dim rectanguloCabeza As New Rectangle(Cabeza.Location, Cabeza.Size)
+            If rectanguloCabeza.IntersectsWith(rectanguloSeccion) Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+
+    Public Function ComioComida() As Boolean
+        Dim rectanguloCabeza As New Rectangle(Cabeza.Location, Cabeza.Size)
+        Dim rectanguloComida As New Rectangle(Comida.Location, Comida.Size)
+        If rectanguloCabeza.IntersectsWith(rectanguloComida) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Sub AgregarSeccionCuerpo(seccion As Label)
+        SeccionesCuerpo.Add(seccion)
+        pnlCampoDeJuego.Controls.Add(seccion)
+    End Sub
+
+    Private Sub GenerarComida()
+        If Comida IsNot Nothing Then
+            Puntaje += 1
+        End If
+
+        Comida = CrearComida()
+
+        pnlCampoDeJuego.Controls.RemoveByKey("Comida")
+        pnlCampoDeJuego.Controls.Add(Comida)
+    End Sub
+
+    Private Function GenerarCoordenadasComida() As Point
+        Dim x, y As Integer
+        Dim areaDeJuego As Size = pnlCampoDeJuego.Size
+        Static random As New Random
+        Dim coordenadas As Point
+        Dim rectanguloTentativoComida As Rectangle
+        Do
+            x = random.Next(1, areaDeJuego.Width - Paso - 1)
+            y = random.Next(1, areaDeJuego.Height - Paso - 1)
+            coordenadas = New Point(x, y)
+            rectanguloTentativoComida = New Rectangle(coordenadas, New Size(Paso, Paso))
+        Loop While AreaComidaCoincideConViborita(rectanguloTentativoComida)
+        Return coordenadas
+    End Function
+
+    Private Function AreaComidaCoincideConViborita(areaComida As Rectangle) As Boolean
+        For Each s As Label In SeccionesCuerpo
+            Dim areaSeccion As New Rectangle(s.Location, s.Size)
+            If areaSeccion.IntersectsWith(areaComida) Then
+                tmrMovimiento.Enabled = False
+                MsgBox("Colision")
+                tmrMovimiento.Enabled = True
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+
+    Private Function CrearLabel(coordenadas As Point) As Label
+        Return New Label With {
+            .Size = New Size(Paso, Paso),
+            .AutoSize = False,
+            .Location = coordenadas,
+            .BorderStyle = BorderStyle.FixedSingle
+        }
+    End Function
+
+    Private Function CrearComida() As Label
+        Dim lbl As Label = CrearLabel(GenerarCoordenadasComida)
+        With lbl
+            .BackColor = Color.DarkRed
+            .Name = "Comida"
+        End With
+        Return lbl
+    End Function
+
+    Private Function CrearSeccionCuerpo(posX As Integer, posY As Integer) As Label
+        Dim lbl As Label = CrearLabel(New Point(posX, posY))
+        lbl.BackColor = Color.MediumAquamarine
+        Return lbl
+    End Function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+End Class
